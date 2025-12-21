d44d50ef8218e4ff251b47e90fba8ddc634cfb45
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2025-12-21 15:04:57 +0530

    updated login.spec.ts test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../Pages/login.page';
-import testData1 from '../Utilities/config.json'
+import testData1 from '../Utilities/config.json';
+import data from '../Utilities/login.json';
 test.use({ storageState: 'Data/login_SF.json' });
 
 /*test.describe.serial(`Login tests using 2 different data set in serial mode`,async () => {
@@ -34,6 +35,7 @@ test(`Login Salesforce using Storage session`, async ({ page }) => {
   const loginPage = new LoginPage(page);
   await page.goto(testData1[1].PostLogin as string);
   await page.waitForTimeout(5000);
+  await loginPage.login(data[0].username, data[0].password);
   // await page.waitForLoadState('networkidle');
   //Created new repository
 })
