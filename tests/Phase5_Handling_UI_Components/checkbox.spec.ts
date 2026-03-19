07bd8194864ccd5448aa4dfc762e8ef09a0dac0a
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2026-03-19 16:54:40 +0530

    Checkbox Automation completed

--- tests/Phase5_Handling_UI_Components/checkbox.spec.ts
+++ tests/Phase5_Handling_UI_Components/checkbox.spec.ts
@@ -1,6 +1,6 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
-test(`Demo QA - Checkbox - Verify page loads successfully`, async({page})=>{
+test(`1. Verify page loads successfully`, async ({ page }) => {
 
     await page.goto(`https://demoqa.com/checkbox`);
     await page.waitForLoadState('networkidle');
@@ -10,17 +10,114 @@ test(`Demo QA - Checkbox - Verify page loads successfully`, async({page})=>{
 })
 
 
-test.only(`Verify no checkbox is selected by default`,async({page})=>
-{
+test(`2. Verify no checkbox is selected by default`, async ({ page }) => {
     await page.goto(`https://demoqa.com/checkbox`);
     await expect(page).toHaveURL(`https://demoqa.com/checkbox`);
     await expect(page.locator(`.rc-tree-checkbox`)).toBeVisible();
 
     await page.locator(`.rc-tree-switcher`).click();
-    const expandedNodes = await page.locator('.rc-tree-treenode').count();
+    const expandedNodes = await page.locator('.rc-tree-checkbox').count();
     expect(expandedNodes).toBeGreaterThan(0);
-    console.log(`The total no of expanded nodes are ${expandedNodes}`);
+    console.log(`There are ${expandedNodes} expanded checkboxes after clicking on the expand checkbox`);
+
+
+    const checkboxes = page.locator(`.rc-tree-checkbox`);
+    const count = await checkboxes.count();
+
+    for (let i = 0; i < count; i++) {
+        const checkbox = checkboxes.nth(i);
+        await expect(checkbox).not.toBeChecked();
+        console.log(`Checkbox ${i + 1} is not selected by default`);
+    }
+    await page.waitForTimeout(5000);
+})
+
+test(`3. Verify selecting single child checkbox`, async ({ page }) => {
+    await page.goto(`https://demoqa.com/checkbox`);
+    await page.waitForLoadState('networkidle');
+    await page.locator(`.rc-tree-switcher`).click();
+    // await page.waitForTimeout(2000);
+    await page.locator(`.rc-tree-switcher`).nth(1).click();
+    await page.locator(`.rc-tree-checkbox`).nth(2).click();
+
+    const notification = page.locator(`//span[text()='notes']`);
+    expect(notification).toBeVisible();
+    await page.waitForTimeout(5000);
+});
+
+
+test(`4. Verify parent checkbox selects all child checkboxes`, async ({ page }) => {
+    await page.goto(`https://demoqa.com/checkbox`);
+    await page.waitForLoadState('networkidle');
+
+    await page.locator(`.rc-tree-switcher`).nth(0).click();
+    await page.locator(`.rc-tree-switcher`).nth(1).click();
+
+    await page.locator(`.rc-tree-checkbox`).nth(1).click();
+
+    await expect(page.locator(`.rc-tree-checkbox`).nth(2)).toBeChecked();
+    await expect(page.locator(`.rc-tree-checkbox`).nth(3)).toBeChecked();
+    await page.waitForTimeout(5000);
+})
+
+test(`5. Verify unchecking child checkbox updates parent state`, async ({ page }) => {
+
+    await page.goto(`https://demoqa.com/checkbox`);
+    await page.waitForLoadState('networkidle');
+
+    await page.locator(`.rc-tree-switcher`).nth(0).click();
+    await page.locator(`.rc-tree-switcher`).nth(1).click();
+
+    await page.locator(`.rc-tree-checkbox`).nth(1).click();
+
+    await page.locator(`.rc-tree-checkbox`).nth(2).click();
+    await page.locator(`.rc-tree-checkbox`).nth(3).click();
+
+    await expect(page.locator(`.rc-tree-checkbox`).nth(1)).not.toBeChecked();   
+
+
     await page.waitForTimeout(5000);
 
-// test
-})
+});
+
+
+test(`6.Verify output section displays correct selected values`, async ({ page }) => {
+
+    await page.goto(`https://demoqa.com/checkbox`);
+    await page.waitForLoadState('networkidle');
+
+    await page.locator(`.rc-tree-switcher`).nth(0).click();
+    await page.locator(`.rc-tree-checkbox`).nth(1).click();
+    // await page.locator(`//span[@class='rc-tree-switcher rc-tree-switcher_open']`).click();
+    // await page.locator(`//span[@class='rc-tree-checkbox rc-tree-checkbox-checked']`).click();
+
+    const output1 = page.locator(`//span[text()='notes']`);
+    await expect(output1).toBeVisible();
+
+    // Get all output texts inside #result
+    const outputTexts = await page.locator('#result span').allTextContents();
+    console.log(`Action : ${outputTexts}`);
+
+    // Optionally, assert the expected values
+    expect(outputTexts).toContain('notes');
+
+    await page.locator(`.rc-tree-checkbox`).nth(1).click();
+    await page.locator(`.rc-tree-checkbox`).nth(2).click();
+    const output2 = page.locator(`//span[text()='documents']`);
+    await expect(output2).toBeVisible();
+    const outputTexts2 = await page.locator('#result span').allTextContents();
+    console.log(`Action : ${outputTexts2}`);
+
+    await page.locator(`.rc-tree-checkbox`).nth(2).click();
+    await page.locator(`.rc-tree-checkbox`).nth(3).click();
+    const output3 = page.locator(`//span[text()='downloads']`);
+    await expect(output3).toBeVisible();
+    const outputTexts3 = await page.locator('#result span').allTextContents();
+    console.log(`Action : ${outputTexts3}`);
+
+    await page.waitForTimeout(5000);
+});
+
+
+
+
